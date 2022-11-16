1e7b4a2ce3e5eb509583fd8525704b1fe51417e8
codergpt <2877797150@qq.com>
2022-11-16 23:16:35 +0800

    vant库的使用

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,68 +1,46 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabbarData" :key="index">
-      <div
-        class="tab-bar-item"
-        :class="{ active: currentIndex === index }"
-        @click="itemClick(index, item)"
-      >
-        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
-        <img v-else :src="getAssetURL(item.imageActive)" alt="">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path">
+          <template #default>
+            <span>{{ item.text }}</span>
+          </template>
+          <template #icon>
+            <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+            <img v-else :src="getAssetURL(item.imageActive)" alt="">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
 
   import {ref} from 'vue'
-  import {useRouter} from 'vue-router'
 
   import tabbarData from '@/assets/data/tabbar.js'
 
   import { getAssetURL } from '@/utils/load_assets.js'
 
   const currentIndex = ref(0)
-  const router = useRouter()
-  const itemClick = (index, item) => {
-    currentIndex.value = index
-    router.push(item.path)
-
-  }
 
 </script>
 
 <style lang="less" scoped>
   .tab-bar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 50px;
-    display: flex;
+    //  局部定义一个变量，只针对.tab-bar子元素才生效
+    //--van-tabbar-item-icon-size: 30px !important;
 
-    border-top: 2px solid #f0f0f0;
+    // 找到类，对类中的css属性重写
+    // :deep(.class/#id)找到子组件的类/id，让子组件的类/id也可以生效
+    :deep(.van-tabbar-item__icon) {
+      font-size: 30px;
+    }
 
-    .tab-bar-item {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-
-      &.active {
-        color: var(--primary-color);
-      }
-
-      img {
-        width: 36px;
-      }
-
-      .text {
-        font-size: 12px;
-        margin-top: 2px;
-      }
+    img {
+      height: 30px;
     }
   }
 </style>
